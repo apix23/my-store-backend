ecd7b560591ec842a1447c836abf4cc91b814e35
Alejandro <sanchezgalejandroj@gmail.com>
2023-10-06 10:48:45 +0200

    Adding a fix path to the url
    in case we handle differents versions in our code we can add
     a fix path to each one by doing this

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
+const express = require("express")
 const productsRoute = require("./products")
 const  usersRoute = require("./users")
 const categoriesRoute = require("./categories")
 
 function routersApi(app) {
-  app.use('/products',productsRoute)
-  app.use('/users',usersRoute)
-  app.use("/categories",categoriesRoute)
+  const route = express.Router()
+  //in case we want to add and extra layer route to the url
+  // by doing this we are adding '/api/v1/' to each one
+  app.use('/api/v1/', route)
+  route.use('/products',productsRoute)
+  route.use('/users',usersRoute)
+  route.use("/categories",categoriesRoute)
 }
 
 module.exports = routersApi
