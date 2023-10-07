715d254d79a9a0c2c48f42d45d0e4487cb86c410
Alejandro <sanchezgalejandroj@gmail.com>
2023-10-07 09:20:50 +0200

    adding a delete and pacth endpoints

--- routes/products.js
+++ routes/products.js
@@ -29,10 +29,39 @@ router.get('/:id', (req,res) => {
 })
 
 router.post('/', (req,res)=>{
-  const body = req.body
+const body = req.body
   res.json({
     message:"created",
     data: body
+  })
+})
+
+/*
+We can use here either a patch or a put as a endpoint
+even though this is possible, for convention we use patch
+when we want to update some properties but not all of then
+
+In case we wanted to eddit all of then, for convention we would be
+using a put
+
+*/
+router.patch('/:id', (req,res)=>{
+const body = req.body
+const {id} = req.params
+  res.json({
+    id,
+    message:`properties changed: ${JSON.stringify(body)}`,
+    body
+  })
+})
+
+
+router.delete('/:id', (req,res)=>{
+const {id} = req.params
+  res.json({
+    id,
+    message:"the element was deleted",
+
   })
 })
 
